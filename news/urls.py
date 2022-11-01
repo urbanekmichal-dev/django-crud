9ca2c64137de96a164936b4ac053c4e87cb23df9
Michal Urbanek <urbanekmichal.98@gmail.com>
2022-11-01 23:36:34 +0100

    add edit features

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import index, add, get, edit
+from .views import index, add, get, edit, update
 
 urlpatterns = [
     path('', index, name="view_news"),
     path('add/', add, name="add"),
     path('<int:id>/', get, name='get'),
     path('edit/<int:id>/', edit, name='get'),
+    path('update/<int:id>/', update, name='update')
 ]
