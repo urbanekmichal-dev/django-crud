e8691243c794197d741bcf2714164e742bd9a59b
Michal Urbanek <urbanekmichal.98@gmail.com>
2022-11-01 20:46:01 +0100

    add news details view

--- home/views.py
+++ home/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
+from news.models import News
+
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'news.html')
--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import get_news
+from django.urls import path, include
+from .views import index, add, get
 
 urlpatterns = [
-    path('news', get_news, name='get_news'),
+    path('', index, name="view_news"),
+    path('add/', add, name="add"),
+    path('<int:id>/', get, name='get'),
 ]
