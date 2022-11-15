8adf72eea78bf27f0767ddf91e4cd656114762e0
Michal Urbanek <urbanekmichal.98@gmail.com>
2022-11-15 22:50:15 +0100

    add CRUD operations to reservations app

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import index, add, get, edit, update
 
 urlpatterns = [
-    path('', index, name="view_news"),
-    path('add/', add, name="add"),
-    path('<int:id>/', get, name='get'),
-    path('edit/<int:id>/', edit, name='get'),
-    path('update/<int:id>/', update, name='update')
+    path('', index, name='view_news'),
+    path('add/', add, name='add_news'),
+    path('<int:id>/', get, name='get_news'),
+    path('edit/<int:id>/', edit, name='edit_news'),
+    path('update/<int:id>/', update, name='update_news')
 ]
